838587e0e69bfac38997f6a35d440fa7109afdce
void-57 <ruddhaani60@gmail.com>
2023-11-11 22:03:12 +0530

    void

--- babelfish.cpp
+++ babelfish.cpp
@@ -6,9 +6,21 @@ int main(){
 ios::sync_with_stdio(false);
 cin.tie(NULL);
 cout.tie(NULL);
-int t;
-cin>>t;
-while(t--){
-
+map<string,string>m;
+string s,t,l;
+while(getline(cin,l)&&!l.empty()){
+    int pos=l.find(' ');
+    
+    s=l.substr(0,pos);
+    t=l.substr(pos+1);
+    m[t]=s;
+    
+}
+string p;
+while(cin>>p){
+    if(m.find(p)==m.end())
+    cout<<"eh"<<endl;
+    else
+    cout<<m[p]<<endl;
 }
 }
